58a3ba89a8bfc11249f4f903cec94728061bb385
soi013 <soi2flatline@gmail.com>
2025-03-31 22:47:53 +0900

    関数追加

--- ts_demo1/src/demo_function.ts
+++ ts_demo1/src/demo_function.ts
@@ -1,3 +1,4 @@
+// 引数があって、返り値もある関数
 function RangeStartEnd(start: number, end: number): number[] {
     const result: number[] = [];
     for (let i = start; i <= end; i++) {
@@ -6,6 +7,24 @@ function RangeStartEnd(start: number, end: number): number[] {
     return result;
 }
 
-
+// デモ RangeStartEnd
 const nums = RangeStartEnd(5, 10);
 console.log(nums.join(","));
+
+// 引数があって、返り値がない関数
+function ThreeTimesPrint(text: string): void {
+    for (let i = 0; i < 3; i++) {
+        console.log(text);
+    }
+}
+
+// デモ ThreeTimesPrint
+ThreeTimesPrint("HEY");
+
+// 引数がなくて、返り値がない関数
+function Hello(): void {
+    console.log("Hello");
+}
+
+// デモ Hello
+Hello();    
