b27a5800f322e72f6d11e79bd32078c714c86ca6
soi013 <soi2flatline@gmail.com>
2025-03-31 22:41:34 +0900

    csv_reader_mapの変数を重複しないように変更

--- ts_demo1/src/csv_reader_map.ts
+++ ts_demo1/src/csv_reader_map.ts
@@ -1,23 +1,23 @@
-type User = {
+type User2 = {
     id: number;
     name: string;
     age: number;
     premiumUser: boolean;
 }
 
-const csvText: string = `id,name,age,premiumUser
+const csvText2: string = `id,name,age,premiumUser
 1,John,30,1
 2,Jane,25,0
 3,Jim,35,1
 `;
 
-function printUsers(users: readonly User[]): void {
+function printUsers2(users: readonly User2[]): void {
     users.forEach(user => {
         console.log(`id: ${user.id}, name: ${user.name}, age: ${user.age}, ${user.premiumUser ? "PREMIUM" : "FREE"}`);
     });
 }
 
-function readCsv(csvText: string): User[] {
+function readCsv2(csvText: string): User2[] {
     return csvText.split("\n")
         .filter(line => line !== "")
         .map(line => line.split(","))
@@ -30,5 +30,5 @@ function readCsv(csvText: string): User[] {
         .filter(user => !isNaN(user.id) && !isNaN(user.age));
 }
 
-const users = readCsv(csvText);
-printUsers(users);
+const users2 = readCsv2(csvText2);
+printUsers2(users2);
