d4d501b2e227154e7feb773d77e5423987812ddf
soi013 <soi2flatline@gmail.com>
2025-04-02 23:20:04 +0900

    add function demo

--- ts_demo1/src/demo_function.ts
+++ ts_demo1/src/demo_function.ts
@@ -27,4 +27,45 @@ function Hello(): void {
 }
 
 // デモ Hello
-Hello();    
+Hello();
+
+// 関数式
+const calcBmi = function (height: number, weight: number): number {
+    return weight / (height ** 2);
+}
+
+// デモ calcBmi
+const bmi = calcBmi(1.75, 75);
+console.log(`BMI is ${bmi}.`);
+
+// 基本的に関数式よりアロー式の方が人気
+// アロー関数
+const calcAreaTriangle = (width: number, height: number): number => {
+    return width * height / 2;
+}
+
+// デモ calcAreaTriangle
+const areaTriangle = calcAreaTriangle(10, 5);
+console.log(`areaTriangle is ${areaTriangle}.`);
+
+// アロー関数の省略記法
+const calcAreaTrapezoid = (upper: number, lower: number, height: number): number =>
+    (upper + lower) * height / 2;
+
+
+// デモ calcAreaTrapezoid
+const areaTrapezoid = calcAreaTrapezoid(6, 4, 3);
+console.log(`areaTrapezoid is ${areaTrapezoid}.`);
+
+// オブジェクトに関数を含める
+const mathObj = {
+    // 円周率
+    pi: 3.14,
+    calcAreaCircle: (radius: number): number => {
+        return radius * radius * mathObj.pi;
+    }
+}
+
+// デモ mathObj
+const areaCircle = mathObj.calcAreaCircle(10);
+console.log(`areaCircle is ${areaCircle}.`);
