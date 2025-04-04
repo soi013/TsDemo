ae58b233c5da54d18f08261d26672904cc49b672
soi013 <soi2flatline@gmail.com>
2025-04-04 23:26:53 +0900

    update function demo

--- ts_demo1/src/demo_function.ts
+++ ts_demo1/src/demo_function.ts
@@ -57,10 +57,33 @@ const calcAreaTrapezoid = (upper: number, lower: number, height: number): number
 const areaTrapezoid = calcAreaTrapezoid(6, 4, 3);
 console.log(`areaTrapezoid is ${areaTrapezoid}.`);
 
+
+type SmartPhone = {
+    name: string;
+    price: number;
+    os: string;
+}
+
+// オブジェクトを返すアロー式は（）で囲む
+const createSmartPhone = (name: string, price: number): SmartPhone => ({
+    name,
+    price,
+    os: name.startsWith("iPhone") ? "iOS" : "Android"
+});
+
+// デモ createSmartPhone
+const iphone = createSmartPhone("iPhone 15", 100_000);
+const android = createSmartPhone("Pixel 8", 60_000);
+
+console.log(`iphone :${iphone.name} ${iphone.price} ${iphone.os}`);
+console.log(`android :${android.name} ${android.price} ${android.os}`);
+
+
 // オブジェクトに関数を含める
 const mathObj = {
     // 円周率
     pi: 3.14,
+    // メソッド記法
     calcAreaCircle: (radius: number): number => {
         return radius * radius * mathObj.pi;
     }
@@ -69,3 +92,46 @@ const mathObj = {
 // デモ mathObj
 const areaCircle = mathObj.calcAreaCircle(10);
 console.log(`areaCircle is ${areaCircle}.`);
+
+
+// 可変長引数
+const allConcat = (pars: string, ...inputs: string[]): string => {
+    let result = inputs[0];
+    for (const input of inputs.slice(1)) {
+        result += pars + input;
+    }
+    return result;
+}
+
+// デモ allConcat
+const result123 = allConcat("+", "1", "2", "3");
+console.log(result123);
+
+const resultABC = allConcat("->", "A", "B", "C", "D");
+console.log(resultABC);
+
+
+// スプレッド構文で配列を展開して渡す
+const inputValues = ["hoge", "fuga", "piyo"];
+const resultHogeFugaPiyo = allConcat("~", ...inputValues);
+console.log(resultHogeFugaPiyo);
+
+const createSmartPhone2 = (name: string, price: number, os: string = "iOS"): SmartPhone => ({
+    name,
+    price,
+    os
+});
+
+// デモ createSmartPhone2
+const iphone2 = createSmartPhone2("iPhone 15", 100_000);
+const android2 = createSmartPhone2("Pixel 8", 60_000, "Android");
+
+console.log(`iphone2 :${iphone2.name} ${iphone2.price} ${iphone2.os}`);
+console.log(`android2 :${android2.name} ${android2.price} ${android2.os}`);
+
+// コールバック関数
+inputValues
+    .map(value => value.toUpperCase())
+    .forEach(value => console.log(value));
+
+
