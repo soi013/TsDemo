2a84bde7f7a1ecb7bac17fa0c9416a2acb376e5e
soi013 <soi2flatline@gmail.com>
2025-04-13 15:05:51 +0900

    add demo class or object or closure

---
+++ ts_demo1/src/demo_class_or_object_or_closure.ts
@@ -0,0 +1,58 @@
+// オブジェクト版スピーカー
+type SpeakerObj = {
+    maker: string;
+    volume: number;
+}
+
+function createSpeakerObj(maker: string, volume: number): SpeakerObj {
+    if (maker === "") {
+        throw new Error("maker is required");
+    }
+
+    return {
+        maker,
+        volume,
+    };
+}
+
+function getSpeakerObjVolume(speakerObj: SpeakerObj): string {
+    return `${speakerObj.maker} has ${speakerObj.volume} watt`;
+}
+
+
+const speakerObj = createSpeakerObj("Sony", 10);
+console.log(`speakerObj: ${speakerObj.maker}, spec: ${getSpeakerObjVolume(speakerObj)}`);
+
+// クラス版スピーカー
+class SpeakerClass {
+    readonly maker: string;
+    readonly volume: number;
+
+    constructor(maker: string, volume: number) {
+        if (maker === "") {
+            throw new Error("maker is required");
+        }
+
+        this.maker = maker;
+        this.volume = volume;
+    }
+
+    getSpec(): string {
+        return `${this.maker} has ${this.volume} watt`;
+    }
+}
+
+const speakerClass = new SpeakerClass("Kenwood", 30);
+console.log(`speakerClass: ${speakerClass.maker}, spec: ${speakerClass.getSpec()}`);
+
+//クロージャーで表現
+function createSpeakerClosure(maker: string, volume: number) {
+    return () => {
+        return `${maker} has ${volume} watt`;
+    }
+}
+
+const getSpeakerSpec = createSpeakerClosure("Bose", 100);
+console.log(`getSpeakerSpec: ${getSpeakerSpec()}`);
+
+
