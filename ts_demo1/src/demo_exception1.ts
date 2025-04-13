a8bdf1bc672678439d017b359b3faa037319a045
soi013 <soi2flatline@gmail.com>
2025-04-13 14:46:36 +0900

    add demo exception

---
+++ ts_demo1/src/demo_exception1.ts
@@ -0,0 +1,19 @@
+console.log("demo_exception1.ts");
+
+const newError = new Error("test error");
+throw newError;
+
+// file:///D:/Documents/GitHub/TsDemo/ts_demo1/dist/demo_exception.js:3
+// const newError = new Error("test error");
+//                  ^
+
+// Error: test error
+//     at file:///D:/Documents/GitHub/TsDemo/ts_demo1/dist/demo_exception.js:3:18
+//     at ModuleJob.run (node:internal/modules/esm/module_job:271:25)
+//     at async onImport.tracePromise.__proto__ (node:internal/modules/esm/loader:578:26)
+//     at async asyncRunEntryPointWithESMLoader (node:internal/modules/run_main:116:5)
+
+// Node.js v22.14.0
+
+// ここには到達しない
+console.log("not reached here");
---
+++ ts_demo1/src/demo_exception2.ts
@@ -0,0 +1,74 @@
+console.log("demo_exception2.ts");
+
+try {
+    console.log("create error");
+    const newError = new Error("test error");
+    console.log("throw error");
+    throw newError;
+
+    // ここには到達しない
+    console.log("not reached here");
+} catch (error) {
+    console.log("catch error");
+    console.log(error);
+    // Error: test error
+    // at file:///D:/Documents/GitHub/TsDemo/ts_demo1/dist/demo_exception2.js:4:22
+    // at ModuleJob.run (node:internal/modules/esm/module_job:271:25)
+    // at async onImport.tracePromise.__proto__ (node:internal/modules/esm/loader:578:26)
+    // at async asyncRunEntryPointWithESMLoader (node:internal/modules/run_main:116:5)
+}
+
+// ここには到達する
+console.log("reached here");
+
+// 大域脱出 深いブロックからtry-catchされるまで脱出し続ける
+try {
+    console.log("try 1");
+    try {
+        console.log("try 2");
+        causeError1();
+    } catch (error) {
+        console.log("catch error 2");
+    }
+} catch (error) {
+    console.log("catch error 1 (not reached)");
+}
+
+function causeError1() {
+    console.log("causeError1 start");
+    causeError2();
+    console.log("causeError1 end (not reached)");
+}
+
+function causeError2() {
+    console.log("causeError2 start");
+    throw new Error("test error");
+    console.log("causeError2 end (not reached)");
+}
+
+// 既存のよくあるエラー発生パターン
+// 0除算は例外でなく無限大になる
+try {
+    console.log("try 0");
+    const arrayEmpty: number[] = [];
+    const result = 1 / arrayEmpty.length;
+    console.log("result", result);
+} catch (error) {
+    console.log("catch error 0 (not reached)");
+}
+
+// 数字でないものを数字に変換しようとする
+try {
+    console.log("try BigInt");
+    const result = BigInt("a");
+    console.log("result", result);
+} catch (error) {
+    console.log("catch error BigInt");
+    console.log(error);
+    // SyntaxError: Cannot convert a to a BigInt
+    // at BigInt (<anonymous>)
+    // at file:///D:/Documents/GitHub/TsDemo/ts_demo1/dist/demo_exception2.js:60:20
+    // at ModuleJob.run (node:internal/modules/esm/module_job:271:25)
+    // at async onImport.tracePromise.__proto__ (node:internal/modules/esm/loader:578:26)
+    // at async asyncRunEntryPointWithESMLoader (node:internal/modules/run_main:116:5)
+}
