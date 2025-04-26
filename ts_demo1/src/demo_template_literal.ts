4f14032be2132a2bf5cb3ae80748be0d063a033a
soi013 <soi2flatline@gmail.com>
2025-04-26 09:25:04 +0900

    add demo literal generic union

--- ts_demo1/src/demo_template_literal.ts
+++ ts_demo1/src/demo_template_literal.ts
@@ -1,6 +1,7 @@
 console.log("# demo_template_literal.ts");
 console.log("\n ## テンプレートリテラル");
 
+// テンプレートリテラルの型。JoJoの名前のみを許す
 type onlyJoJo = `ジョ${string}=ジョ${string}`
 
 const joJo1: onlyJoJo = "ジョナサン=ジョースター";
@@ -31,4 +32,42 @@ function getJoJo(generation: number): `ジョ${string}=ジョ${string}` {
 // 呼び出し側が関数の返り値の法則性を知れる
 // function getJoJo(generation: number): `ジョ${string}=ジョ${string}`
 const joJo6 = getJoJo(1);
+
+//ジェネリック型と組み合わせる
+console.log("\n ## テンプレートリテラルとジェネリック型");
+
+function getJojoFullNameNormal(firstName: string) {
+    return `${firstName}=ジョースター`;
+}
+function getJojoFullNameGeneric<T extends string>(firstName: T) {
+    return `${firstName}=ジョースター` as const;
+}
+
+// どちらも結果は同じだが型が違う
+// const jojoFullName1: string
+const jojoFullName1 = getJojoFullNameNormal("ジョナサン");
+
+// const jojoFullName2: "ジョナサン=ジョースター"
+const jojoFullName2 = getJojoFullNameGeneric("ジョナサン");
+
+console.log(jojoFullName1);
+console.log(jojoFullName2);
+
+// ユニオン型との組み合わせ。これがリテラル型の最も多い使い方
+console.log("\n ## テンプレートリテラルとユニオン型");
+
+type JojoName = "ジョナサン" | "ジョセフ" | "ジョルノ";
+
+function getJojoFullNameUnion(firstName: JojoName) {
+    return `${firstName}=ジョースター` as const;
+}
+
+const jojoFullName3 = getJojoFullNameUnion("ジョナサン");
+const jojoFullName4 = getJojoFullNameUnion("ジョセフ");
+const jojoFullName5 = getJojoFullNameUnion("ジョルノ");
+
+console.log(jojoFullName3);
+console.log(jojoFullName4);
+console.log(jojoFullName5);
+
 
