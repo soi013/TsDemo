81b4d067252746a58bb7b9c3e69d26df1bfd08bf
soi013 <soi2flatline@gmail.com>
2025-05-01 00:33:02 +0900

    add demo keyof

--- ts_demo1/src/demo_lookup_keyof.ts
+++ ts_demo1/src/demo_lookup_keyof.ts
@@ -25,4 +25,23 @@ console.log(fentanyl1);
 console.log(fentanyl2);
 
 
+console.log("\n ## keyofでキーの型を取得");
+
+type KeyOfMedicine = keyof Medicine;
+let key: KeyOfMedicine = "price";
+key = "name";
+
+// keyof Medicineは"price" | "name"の型
+// 型 '"hoge"' を型 'keyof Medicine' に割り当てることはできません。ts(2322)
+// key = "hoge";
+
+function keyString(key: KeyOfMedicine) {
+    return `Key is ${key}` as const;
+}
+
+// keyS: "Key is price" | "Key is name"
+const keyS1 = keyString("name");
+const keyS2 = keyString("price");
+console.log(keyS1);
+console.log(keyS2);
 
