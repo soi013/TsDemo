aef8cae13e80bd18a66f6c7515244b4c091eeb4c
soi013 <soi2flatline@gmail.com>
2025-05-03 15:23:52 +0900

    split tupple demo

--- ts_demo1/src/demo_array.ts
+++ ts_demo1/src/demo_array.ts
@@ -5,12 +5,13 @@ console.log("\n ## 配列のデモ");
 const numbers = [1, 2, 3];
 console.log(`numbers: ${numbers}`);
 
-//いろいろな型の配列  something: (string | number | boolean)[]
+console.log("\n ## いろいろな型の配列");
 const something = [1, "two", true];
 
 console.log(`something: ${something}`);
 
 // 配列でもスプレッド構文が使える
+console.log("\n ## 配列でもスプレッド構文が使える");
 const numbers2 = [...numbers, ...numbers, 100, 101];
 console.log(`numbers2: ${numbers2}`);
 
@@ -22,79 +23,51 @@ numbers[2] = 1000;
 console.log(`numbers: ${numbers}`);
 // numbers = [100, 101, 102]; //compile error
 
-// Array<number> と number[] は同じ
+console.log("\n ## Array<number> と number[] は同じ");
 const numbers3: Array<number> = [1, 2, 3];
 console.log(`numbers3: ${numbers3}`);
 
-// 読み取り専用の配列
+console.log("\n ## 読み取り専用の配列");
 const fixedNumbers: readonly number[] = [10, 20, 30];
 // fixedNumbers[2] = 1000; //compile error
 
-// push メソッド
+console.log("\n ## push メソッド");
 numbers.push(4);
 console.log(`numbers: ${numbers}`);
 
-// pop メソッド
+console.log("\n ## pop メソッド");
 const lastNumber = numbers.pop();
 console.log(`numbers: ${numbers}, lastNumber: ${lastNumber}`);
 
-// includes メソッド
+console.log("\n ## includes メソッド");
 console.log(`numbers.includes(100): ${numbers.includes(100)}`);
 console.log(`numbers.includes(2): ${numbers.includes(2)}`);
 
 // fixedNumbers.push(40); //compile error 読み取り専用の配列にはpushメソッドがない
 console.log(`fixedNumbers.includes(10): ${fixedNumbers.includes(10)}`);
 
-// 配列の長さ
+console.log("\n ## 配列の長さ");
 console.log(`numbers.length: ${numbers.length}`);
 
-// indexOf メソッド
+console.log("\n ## indexOf メソッド");
 console.log(`numbers.indexOf(2): ${numbers.indexOf(2)}`);
 console.log(`numbers2.indexOf(2): ${numbers2.indexOf(2)}`);
 
-// 配列のコピー
+console.log("\n ## 配列のコピー");
 const numbers4 = numbers.slice();
 console.log(`numbers4: ${numbers4}`);
 
-// for-of 文
+console.log("\n ## for-of 文");
 for (const number of numbers) {
     console.log(`for-of: number: ${number}`);
 }
 
-// for-in 文
+console.log("\n ## for-in 文");
 for (const index in numbers) {
     console.log(`for-in: index: ${index}, number: ${numbers[index]}`);
 }
 
-// forEach メソッド
+console.log("\n ## forEach メソッド");
 numbers.forEach((number) => {
     console.log(`forEach: number: ${number}`);
 });
-
-// Tupleは配列の一種 : [number, string, boolean]
-const tuple: [number, string, boolean] = [1, "two", true];
-console.log(`tuple: ${tuple}`);
-console.log(`tuple[0]: ${tuple[0]}, tuple[1]: ${tuple[1]}, tuple[2]: ${tuple[2]}`);
-
-// console.log(`tuple[99]: ${tuple[99]}`); //undefined '3' のタプル型 '[number, string, boolean]' にインデックス '99' の要素がありません。ts(2493)
-
-
-// 配列の要素を展開する
-const [first, second, third] = tuple;
-console.log(`first: ${first}, second: ${second}, third: ${third}`);
-
-// ラベル付きタプル : [uno: number, dos: string, tres: boolean]
-const tupleWithLabel: [uno: number, dos: string, tres: boolean] = [1, "two", true];
-console.log(`tupleWithLabel: ${tupleWithLabel}`);
-// console.log(`tupleWithLabel.uno: ${tupleWithLabel.uno}`); //compile error ラベル付きタプルはオブジェクトと同じようにアクセスできない
-
-// オブジェクトでも同じことができるから、だいたいオブジェクトを使う
-const objThree = {
-    first: 1,
-    second: "two",
-    third: true,
-};
-
-console.log(`objThree: ${objThree.first}, ${objThree.second}, ${objThree.third}`);
-
-
---
+++ ts_demo1/src/demo_tupple.ts
@@ -0,0 +1,74 @@
+console.log("# demo_tupple.ts");
+console.log("\n ## タプルのデモ");
+
+console.log("\n ## Tupleは配列の一種");
+const tuple: [number, string, boolean] = [1, "two", true];
+console.log(`tuple: ${tuple}`);
+console.log(`tuple[0]: ${tuple[0]}, tuple[1]: ${tuple[1]}, tuple[2]: ${tuple[2]}`);
+
+// console.log(`tuple[99]: ${tuple[99]}`); //undefined '3' のタプル型 '[number, string, boolean]' にインデックス '99' の要素がありません。ts(2493)
+
+
+console.log("\n ## 配列の要素を展開する");
+const [first, second, third] = tuple;
+console.log(`first: ${first}, second: ${second}, third: ${third}`);
+
+console.log("\n ## ラベル付きタプル : [uno: number, dos: string, tres: boolean]");
+const tupleWithLabel: [uno: number, dos: string, tres: boolean] = [1, "two", true];
+console.log(`tupleWithLabel: ${tupleWithLabel}`);
+// console.log(`tupleWithLabel.uno: ${tupleWithLabel.uno}`); //compile error ラベル付きタプルはオブジェクトと同じようにアクセスできない
+
+console.log("\n ## でもオブジェクトでも同じことができるから、だいたいオブジェクトを使う");
+const objThree = {
+    first: 1,
+    second: "two",
+    third: true,
+};
+
+console.log(`objThree: ${objThree.first}, ${objThree.second}, ${objThree.third}`);
+
+console.log("\n ## 可変長タプル");
+type LongHeadStringAndNumbers = [string, ...number[]];
+
+let evenNumbers: LongHeadStringAndNumbers = ["偶数", 2, 4, 6];
+console.log({ evenNumbers });
+
+evenNumbers.push(8, 10);
+console.log({ evenNumbers });
+
+// 型 '[string, string, number, number, number]' を型 'LongHeadStringAndNumbers' に割り当てることはできません。
+// ソースの位置 1 から 4 にある型は、ターゲットの位置 1 にある型と互換性がありません。
+// 型 'string' を型 'number' に割り当てることはできません。ts(2322)
+// let oddPrimeNumbers: LongHeadStringAndNumbers = ["奇数", "素数", 2, 3, 5];
+
+// ...スプレッド構文の部分は無くてもOK
+let zeroNumber: LongHeadStringAndNumbers = ["zero"];
+console.log({ zeroNumber });
+
+// スプレッド構文ではない部分は指定が必要
+//型 '[]' を型 'LongHeadStringAndNumbers' に割り当てることはできません。
+// ソースには 0 個の要素が含まれていますが、ターゲットには 1 個が必要です。ts(2322)
+// let emptyNumber: LongHeadStringAndNumbers = [];
+// console.log({ emptyNumber });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
