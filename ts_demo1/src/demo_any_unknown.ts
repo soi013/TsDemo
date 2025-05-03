8f3171372347f8ec2720f98623869df40d090f3b
soi013 <soi2flatline@gmail.com>
2025-05-03 11:36:00 +0900

    add demo unknown

--- ts_demo1/src/demo_any_unknown.ts
+++ ts_demo1/src/demo_any_unknown.ts
@@ -33,8 +33,43 @@ console.log({ anythingInput });
 anythingInput = { jojo: "ジョセフ=ジョスター", statge: 2 };
 console.log({ anythingInput });
 
+console.log("\n ## unknown型のデモ");
 
+function doAlmostNothing(arg: unknown) {
+    console.log(arg);
 
+    // anyと異なり、コンパイルエラーが起きる
+    // 'arg''は 'unknown' 型です。ts(18046)
+    // arg.notExistingMethod();
+
+    // anyと異なり、コンパイルエラーが起きる
+    // 'arg''は 'unknown' 型です。ts(18046) 
+    // arg.notExistingProperty1 = arg.notExistingProperty2 * 99;
+
+    // 型で絞り込めば、プロパティアクセスなどができる
+    if (typeof arg === "string") {
+        console.log(`arg is string. Upper arg = [${arg.toUpperCase()}]`);
+    }
+
+    if (typeof arg === "object" && arg !== null && "hoge" in arg) {
+        console.log(`arg is object. arg.hoge = [${arg.hoge}]`);
+    }
+}
+
+//どんな引数も渡せる
+doAlmostNothing("Hello");
+doAlmostNothing(123);
+doAlmostNothing({ hoge: "HOGE", fuga: 123 });
+
+// unknown型にもなんでも入れられる
+let unknownInput: unknown = "Hello";
+console.log({ unknownInput });
+unknownInput = 123;
+console.log({ unknownInput });
+unknownInput = true;
+console.log({ unknownInput });
+anythingInput = { jojo: "ジョセフ=ジョスター", statge: 2 };
+console.log({ anythingInput });
 
 
 
