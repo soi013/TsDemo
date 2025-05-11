049983501e3dc44d43a9ac1322b5615d71431e7a
soi013 <soi2flatline@gmail.com>
2025-05-11 09:37:32 +0900

    add demo function export import

--- ts_demo1/src/demo_module_export.ts
+++ ts_demo1/src/demo_module_export.ts
@@ -1,10 +1,19 @@
 console.log("# demo_module_export.ts");
 console.log("\n ## モジュールの使い方 export側");
 
+// exportをつけると他のファイルから参照できる
 export const bookTitle = "Steel Ball Run"
 export const bookAuthor = "Hirohiko Araki"
+
+// exportをつけないと他のファイルから参照できない
 const secretName = "Jojo 6th";
 
+// 名前を変えてエクスポートする
+export { secretName as Himitsu };
 
+// 関数をエクスポートする
+export function getBookInfo() {
+    return `(${bookTitle}) by (${bookAuthor})`;
+}
 
 
--- ts_demo1/src/demo_module_import.ts
+++ ts_demo1/src/demo_module_import.ts
@@ -3,12 +3,20 @@ console.log("\n ## モジュールの使い方 import側");
 
 // モジュール '"./demo_module_export"' は 'secretName' をローカルで宣言していますが、これはエクスポートされていません。ts(2459)
 // import { bookTitle, bookAuthor, secretName } from "./demo_module_export";
-import { bookTitle, bookAuthor, } from "./demo_module_export.js";
+import { bookTitle, getBookInfo, Himitsu } from "./demo_module_export.js";
 
-console.log(bookTitle, bookAuthor);
+// as で名前を変えてインポートする
+import { bookAuthor as BA } from "./demo_module_export.js";
+
+console.log(`the book title is [${bookTitle}] and the author is [${BA}].`);
+
+// export していない変数は参照できない
 // 名前 'secretName' が見つかりません。ts(2304)
 // console.log(secretName);
 
+console.log(`the secret name is [${Himitsu}].`);
+
+console.log(getBookInfo());
 
 
 
