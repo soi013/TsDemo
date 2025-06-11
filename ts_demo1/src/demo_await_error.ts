547e9cc05980aa8cd307693a6fbef0853cf8f70a
soi013 <soi2flatline@gmail.com>
2025-06-11 21:51:12 +0900

    add demo await error

---
+++ ts_demo1/src/demo_await_error.ts
@@ -0,0 +1,35 @@
+import { performance } from 'perf_hooks';
+
+console.log("# demo_await_error.ts");
+
+// awaitでのエラー処理
+console.log("\n ## awaitでのエラー処理");
+
+const startTime = performance.now();
+
+try {
+    const readedText = await readUnknownFile();
+    console.log(`3A. end____: read unknown file await ${performance.now() - startTime}ms`);
+    console.log(`4A. readedText: ${readedText.slice(0, 10)}`);
+} catch (error) {
+    console.log(`3B. end____: read unknown file await ${performance.now() - startTime}ms`);
+    console.log('4B. ERROR:', error);
+}
+
+
+console.log(`5. end____: read unknown file await ${performance.now() - startTime}ms`);
+
+
+async function readUnknownFile(): Promise<string> {
+    const targetFilePath = 'unknown_file.txt';
+
+    console.log(`1. start__: read unknown file await ${performance.now() - startTime}ms`);
+
+    const { readFile } = await import('fs/promises');
+
+    console.log(`2. started: import readfile unknown file completed ${performance.now() - startTime}ms`);
+
+    const readedText = await readFile(targetFilePath, 'utf-8');
+    return readedText;
+}
+
