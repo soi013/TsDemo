44a31d8ca34078343fddeee5971dab0c9a43883e
soi013 <soi2flatline@gmail.com>
2025-06-13 22:43:23 +0900

    update awai error

--- ts_demo1/src/demo_await_error.ts
+++ ts_demo1/src/demo_await_error.ts
@@ -7,18 +7,28 @@ console.log("\n ## awaitでのエラー処理");
 
 const startTime = performance.now();
 
-try {
-    const readedText = await readUnknownFile();
-    console.log(`3A. end____: read unknown file await ${performance.now() - startTime}ms`);
-    console.log(`4A. readedText: ${readedText.slice(0, 10)}`);
-} catch (error) {
-    console.log(`3B. end____: read unknown file await ${performance.now() - startTime}ms`);
-    console.log('4B. ERROR:', error);
+const result = readUnknownFileCatch()
+    .then(() => {
+        console.log(`6. end____: read unknown file then ${performance.now() - startTime}ms`);
+    });
+
+await result;
+
+console.log(`7. end____: await final result ${performance.now() - startTime}ms`);
+
+async function readUnknownFileCatch() {
+    try {
+        const readedText = await readUnknownFile();
+        console.log(`3A. end____: read unknown file await ${performance.now() - startTime}ms`);
+        console.log(`4A. readedText: ${readedText.slice(0, 10)}`);
+    } catch (error) {
+        console.log(`3B. end____: read unknown file await ${performance.now() - startTime}ms`);
+        console.log('4B. ERROR:', error);
+    }
+
+    console.log(`5. end____: read unknown file await ${performance.now() - startTime}ms`);
+
 }
-
-
-console.log(`5. end____: read unknown file await ${performance.now() - startTime}ms`);
-
 
 async function readUnknownFile(): Promise<string> {
     const targetFilePath = 'unknown_file.txt';
